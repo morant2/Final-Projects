279d1767dac88a0e8a66bb113007bc5a071304d1
tsm100 <tylersmoran125@gmail.com>
2023-03-09 13:57:59 -0500

    added NavBar and Login

--- FinalProjects/HealthApp/src/router/index.ts
+++ FinalProjects/HealthApp/src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import { useSession } from '@/Model/session'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
